42a3ef9ea96cc7ef7113839b1a771bfaca125ce8
Dario Varotto <dariosky@gmail.com>
2020-10-17 19:26:04 +0200

    WSGI overriding system sqlite via pysqlite3

---
+++ webservice/wsgi.py
@@ -0,0 +1,10 @@
+# if you have an old pysqlite version in your system:
+# install `pip install pysqlite3-binary`
+# patch the sqlite3 module to use pysqlite3 - to override old system sqlite
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+# noinspection PyUnresolvedReferences
+from webservice.apiserver import app as application
